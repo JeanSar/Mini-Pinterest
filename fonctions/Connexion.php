f333626a38e42f71648d1b73499a98bb5f2b2e16
DELOIN GASPAR p1806858 <gaspar.deloin@etu.univ-lyon1.fr>
2020-04-06 21:36:58 +0200

    Replace Connexion.php

--- fonctions/Connexion.php
+++ fonctions/Connexion.php
@@ -1,24 +1,22 @@
 <?php
 
-$dbHost = "localhost";// à compléter
-$dbUser = "root";// à compléter
-$dbPwd = "";// à compléter
-$dbName = "Pinterest";
-
 /*Cette fonction prend en entrée l'identifiant de la machine hôte de la base de données, les identifiants (login, mot de passe) d'un utilisateur autorisé 
 sur la base de données contenant les tables pour le chat et renvoie une connexion active sur cette base de donnée. Sinon, un message d'erreur est affiché.*/
-function getConnection($dbHost, $dbUser, $dbPwd, $dbName)
+function getConnection()
 {
-
+	$dbHost = "localhost";// à compléter
+	$dbUser = "root";// à compléter
+	$dbPwd = "";// à compléter
+	$dbName = "mini-pinterest";
 	$link = mysqli_connect($dbHost, $dbUser, $dbPwd, $dbName);
 	if (!$link) {
 		echo "Erreur : Impossible de se connecter à MySQL." . PHP_EOL;
 	        echo "Erreur de débogage : " . mysqli_connect_errno() . PHP_EOL;
 	        echo "Erreur de débogage : " . mysqli_connect_error() . PHP_EOL;
 		exit;
-	echo "Succès : Une connexion correcte à MySQL a été faite! La base de donnée chat est au point." . PHP_EOL;
-	echo "Information d'hôte : " . mysqli_get_host_info($link) . PHP_EOL;
-	return $link;
+	}
+	else{
+		return $link;
 	}
 }
 
@@ -28,18 +26,21 @@ qu'une requête SQL SELECT et renvoie les résultats de la requête. Si le résu
 function executeQuery($link, $query)
 {
  
-	 if ($result = mysqli_query($link, $query, MYSQLI_USE_RESULT)) {
-		if (!mysqli_query($link, "SET @a:='this will not work'")) {
-			printf("Erreur : %s\n", mysqli_error($link));
+	 if ($resultat = mysqli_query($link, $query, MYSQLI_USE_RESULT)) {
+		 	return $resultat;
+			mysqli_free_result($res);
 		}
 		else {
-		while($row = mysqli_fetch_array($result)) {
-			print_r($row);
+			printf("Erreur : %s\n", mysqli_error($link));
 		}
-	}
-	mysqli_free_result($result);
-	}
+}
 
+function tableauQuery($requete){
+	$tab = array();
+	while ($resultat = mysqli_fetch_array($requete)){
+		$tab[]=$resultat[0];
+	}
+	return $tab;
 }
 
 /*Cette fonction prend en entrée une connexion vers la base de données du chat ainsi 
