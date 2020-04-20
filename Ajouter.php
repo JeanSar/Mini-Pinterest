7fa22bc4109e4685c227235f63d5b28631584239
p1806858 <hipole@Wowtwinka.localdomain>
2020-04-20 15:13:40 +0200

    modification accueil pour résultat

---
+++ Ajouter.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Quelle photo ?</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<br />
+	
+	<textarea id="story" name="story"
+          rows="5" cols="33">
+		It was a dark and stormy night...
+	</textarea>
+	
+	<br />
+	
+	<INPUT type=file> </INPUT>
+	
+	<br />
+
+	<select id="categorie" name="categorie">
+		<option value="Fruit">Fruit</option>
+		<option value="Légume">Légume</option>
+  		<option value="fiat"></option>
+		<option value="audi">Audi</option>
+	</select>
+	<input type="submit">
+</body>
+</html>
